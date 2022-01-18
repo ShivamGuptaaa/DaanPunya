7dc94d17eaf321fc4f3da9ae94b7af5547469f26
Shivam Gupta <44132332+ShivamGuptaaa@users.noreply.github.com>
2022-01-18 16:08:40 +0530

    Update settings.py

--- DaanPunya/DaanPunya/settings.py
+++ DaanPunya/DaanPunya/settings.py
@@ -58,8 +58,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'daanpunya123@gmail.com'
-EMAIL_HOST_PASSWORD = 'daanpunya@12345'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 TEMPLATES = [
     {
